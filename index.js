c73db50873933f5a2337fa267301477e713d6d25
muhammadzaki2007 <mzakiw2007@gmail.com>
2024-07-24 14:55:41 +0700

    zaki: mengubah index

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ app.put("/test/:name", (req, res) => {
     res.json(data)
   });
 
+  app.post("/zaki/:name", (req, res) => {
+    console.log(req.params.name);
+    const data = {
+        nama : req.body.nama,
+        pekerjaan : req.body.job
+    }
+    // console.log("Hello world");
+    res.json(data)
+  }); 
+
 const PORT = process.env.PORT || 8000; // mengakses port dari setingan env pada laptop
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
